902fb0497cc4314bc031449e934b55652e69f82f
Matvey Bouhartsev <programmer@bouhartsev.site>
2021-07-05 01:05:24 +0300

    Actions done

--- kanban/src/App.vue
+++ kanban/src/App.vue
@@ -81,27 +81,11 @@ export default {
 @import url("~@/assets/styles/style.css");
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  max-width: 2000px;
+  margin: auto;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
-.overflow-hidden {
-  overflow: hidden;
+body {
+  overflow: overlay;
 }
 </style>
--- kanban/src/components/Lists.vue
+++ kanban/src/components/Lists.vue
@@ -27,11 +27,11 @@
         <i class="text">{{ item.date }}</i>
         <button
           class="check"
-          @click="changePos(item.id, 2)"
+          @click="changePos(item.id, list.id+1)"
           v-if="list.id != 2"
         ></button>
         <button class="cross" @click="deleteItem(item.id)" v-else></button>
-        <button class="pencil"></button>
+        <button class="pencil" @click="editItem(item.id)"></button>
       </div>
     </div>
   </section>
@@ -116,22 +116,75 @@ export default {
       const itemId = parseInt(e.dataTransfer.getData("itemId"));
       this.changePos(itemId, listId);
     },
+    getDate: function() {
+      let date = new Date();
 
+      let dd = date.getDate();
+      if (dd < 10) dd = '0' + dd;
+
+      let mm = date.getMonth() + 1;
+      if (mm < 10) mm = '0' + mm;
+
+      let yy = date.getFullYear() % 100;
+      if (yy < 10) yy = '0' + yy;
+
+      let h = date.getHours();
+      if (h < 10) h = '0' + h;
+
+      let m = date.getMinutes();
+      if (m < 10) m = '0' + m;
+
+      return dd + '.' + mm + '.' + yy + ' ' + h + ':' + m;
+    },
     saveItem: function () {
-        console.log('save');
-    //   let input_id = (document.getElementById("taskID").value!="-1") ? document.getElementById("taskID").value : this.itemsData[this.itemsData.length-1].id + 1;
-    //   let input_title = document.getElementById("taskText").value;
-    //   let today = new Date();
-    //   let input_date = today; //change!
-    //   let input_priority = document.getElementById("input_priority").value;
-    //   let input_listId = document.getElementById("taskList").value;
-    //   this.itemsData.push({
-    //     id: input_id,
-    //     title: input_title,
-    //     priority: input_priority,
-    //     date: input_date,
-    //     listId: 0,
-    //   });
+        let input_title = document.getElementById("taskText").value;
+        let input_date = this.getDate();
+        let input_priority = Number(document.getElementById("taskPriority").value);
+        let input_listId = Number( document.getElementById("taskList").value);
+        let input_item = {
+            title: input_title,
+            priority: input_priority,
+            date: input_date,
+            listId: input_listId,
+        };
+        if (document.getElementById("taskID").value!="-1") {
+          let input_id = document.getElementById("taskID").value;
+          input_item.id = input_id;
+          this.itemsData = this.itemsData.map((x) => {
+            if (x.id == input_id) x = input_item;
+            return x;
+          });
+        }
+        else {
+          let input_id = this.itemsData[this.itemsData.length-1].id + 1;
+          input_item.id = input_id;
+          this.itemsData.push(input_item);
+          this.changelength();
+        }
+    },
+    editItem: function (input_id=-1) {
+      let input_title = '';
+      let input_priority = 0;
+      let input_listId = 0;
+
+      this.itemsData.map((x) => {
+        if (x.id==input_id) {
+          input_title = x.title;
+          input_priority = x.priority;
+          input_listId = x.listId;
+        }
+      });
+
+      this.$parent.$refs.modalTask.openModal();
+
+      setTimeout(function(){
+        document.getElementById("taskID").value=input_id;
+        document.getElementById("taskText").value=input_title;
+        document.getElementById("taskPriority").value=input_priority;
+        document.getElementById("taskList").value=input_listId;
+      },100)
+
+      
     },
     deleteItem: function (itemId) {
       this.itemsData = this.itemsData.filter((x) => x.id != itemId);
@@ -154,6 +207,15 @@ export default {
   },
   mounted() {
     this.changelength();
+  },
+  created() {
+    // console.log(localStorage['kanbanData'], JSON.parse(localStorage['kanbanData']));
+    if (localStorage['kanbanData']) this.itemsData = JSON.parse(localStorage['kanbanData']);
+  },
+  watch: {
+    itemsData: function (val) {
+      localStorage.setItem(['kanbanData'], JSON.stringify(val));
+    },
   },
 };
 </script>
